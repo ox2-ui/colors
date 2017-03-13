46a00b942d9c5734c861b4ffc4b7081ef080d160
Lauricio Su <me@lauricio.com>
2017-03-13 11:59:46 +0700

    :space_invader: :space_invader: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# @ox2/colors
+![NPM version](https://img.shields.io/badge/npm-private-orange.svg?style=flat)
+<!-- ![NPM version](https://img.shields.io/npm/v/@ox2/colors.svg?style=flat) -->
+
+Ui color palette.
+
+## Installation
+Install using [npm](http://npmjs.com):
+```sh
+npm install @ox2/colors --save
+```
+Install using [yarn](http://yarnpkg.com):
+```sh
+yarn add @ox2/colors
+```
+
+## Usage
+```js
+import { critical } from '@ox2/colors'
+
+```
+
+## Change Log
+Changes are tracked in the [CHANGELOG.md](https://github.com/ox2/colors/blob/master/CHANGELOG.md)
+
+## License
+[MIT](https://github.com/ox2/colors/blob/master/LICENSE)
---
+++ src/index.js
@@ -0,0 +1,35 @@
+// Neutral colors
+export const neutral_tint = 'hsla(0, 0%, 96%, 1)';
+export const neutral_faded = 'hsla(0, 0%, 86%, 1)';
+export const neutral = 'hsla(0, 0%, 50%, 1)';
+export const neutral_light = 'hsla(0, 0%, 67%, 1)';
+export const neutral_dim = 'hsla(0, 0%, 35%, 1)';
+export const neutral_dark = 'hsla(0, 0%, 30%, 1)';
+export const dark = 'hsla(0, 0%, 0%, .1)';
+export const light = 'hsla(0, 100%, 100%, .15)';
+export const light_faded = 'hsla(0, 100%, 100%, .08)';
+export const transparent = 'rgba(0, 0, 0, 0)';
+export const black = 'hsla(0, 0%, 0%, 1)';
+export const white = 'hsla(360, 100%, 100%, 1)';
+
+// UI Element colors
+export const item = 'hsla(204, 33%, 97%, 1)';
+export const backdrop = 'hsla(204, 14%, 89%, 1)';
+export const backdrop_tint = 'hsla(204, 10%, 87%, 1)';
+export const item_alt = 'hsla(204, 8%, 76%, 1)';
+export const backdrop_alt = 'hsla(204, 5%, 23%, 1)';
+
+// Status colors
+export const action = 'hsla(145, 63%, 42%, 1)';
+export const warning = 'hsla(37, 90%, 51%, 1)';
+export const attention = 'hsla(48, 89%, 55%, 1)';
+export const critical = 'hsla(6, 63%, 46%, 1)';
+export const positive = 'hsla(214, 70%, 53%, 1)';
+export const negative = 'hsla(39, 100%, 50%, 1)';
+
+// Social network colors
+export const twitter = '#00ACED';
+export const facebook = '#3B5998';
+export const linkedin = '#007FB1';
+export const google = '#D14836';
+
